b8e4c3517bc26d9d1ce201e3902ccb7e0a976e1f
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-10 14:49:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,6 +17,7 @@ function init() {
   document.body.addEventListener('keydown', function(e)
   {
     indx = (codes[indx] === e.key) ? ++indx : 0
+    console.log(indx)
     
     if (indx === codes.length)
     {
